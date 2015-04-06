3ef7533386045d5b87d634c54a9de006dc4c4d5f
rezkyparanata <rezky.paranata@gmail.com>
2015-04-07 00:14:35 +0700

    versi pertama

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld;
+
+/**
+ *
+ * @author USER
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String nama="Rezky Paranata.";
+       String nim="6306130037.";
+       
+        System.out.println(" Hello, saya " + nama + " Nim saya " + nim + " Saya sangat menyukai praktikum RPL Modul 8 ini. " );
+    }
+    
+}
